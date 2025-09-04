38daf838968d15a06ffdd3d7a20021da37f9a7ca
alex5 <angrymarshall@gmail.com>
2025-09-04 12:46:08 +0300

    metrika

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,32 @@ function App() {
   return (
       <section className="mx-auto max-w-3xl py-12">
           <div className="flex flex-col">
-              <article className="bg-neutral-100 flex flex-col gap-2 p-2 rounded-xl">
+              <article className="border flex flex-col gap-2 p-2 rounded-xl">
                   <b>16</b>
                   <p>Глюкоза (Glucose) A09.05.023.000.01</p>
+                  <b className="font-bold">Рекомендации</b>
                   <ul>
-                      <li className="font-bold">Рекомендации</li>
                       <li>
-                          <button className="add-recommendation">18. HbA1c (гликированный Hb)</button>
+                          <button className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                              18. HbA1c (гликированный Hb)
+                          </button>
                       </li>
                       <li >
-                          <button className="add-recommendation">119. Лейкоцитарная формула</button>
+                          <button className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                              119. Лейкоцитарная формула
+                          </button>
                       </li>
                       <li>
-                          <button className="add-recommendation">11. Альфа-амилаза</button>
+                          <button className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                              11. Альфа-амилаза
+                          </button>
                       </li>
                       <li>
-                          <button className="add-recommendation">ОБС73. Биохимия крови, расш.</button>
+                          <button className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                              ОБС73. Биохимия крови, расш.
+                          </button>
                       </li>
                   </ul>
-                  <span>3 аналога</span>
               </article>
           </div>
       </section>
