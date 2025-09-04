38818685a2a8cf06b1a0c6fc6228f5407e13ab89
alex5 <angrymarshall@gmail.com>
2025-09-04 20:01:56 +0300

    metrika

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,79 @@
 import './App.css'
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {Plus} from "lucide-react";
 
 function App() {
   return (
       <section className="mx-auto max-w-3xl py-12">
           <div className="flex flex-col gap-6">
               <div className="border p-2 rounded-xl">
-                  <b>Офис;</b>
+                  <b>Офис:</b>
               </div>
-              <hr/>
               <div className="flex flex-col gap-6">
                   <article data-productid={16} id="product" className="border flex flex-col gap-2 p-2 rounded-xl">
                       <b>16</b>
                       <p>Глюкоза (Glucose) A09.05.023.000.01</p>
-                      <b className="font-bold">Рекомендации</b>
-                      <ul>
-                          <li>
-                              <button data-recommendationid={18} id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
-                                  18. HbA1c (гликированный Hb)
-                              </button>
-                          </li>
-                          <li >
-                              <button data-recommendationid={119} id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
-                                  119. Лейкоцитарная формула
-                              </button>
-                          </li>
-                          <li>
-                              <button data-recommendationid={11} id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
-                                  11. Альфа-амилаза
-                              </button>
-                          </li>
-                          <li>
-                              <button data-recommendationid={"ОБС73"} id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
-                                  ОБС73. Биохимия крови, расш.
-                              </button>
-                          </li>
-                      </ul>
+                      <b className="font-bold"></b>
+                      <Dialog>
+                          <DialogTrigger asChild>
+                              <Button>3 рекомендации</Button>
+                          </DialogTrigger>
+                          <DialogContent>
+                              <DialogHeader>
+                                  <DialogTitle>Добавить рекомендации</DialogTitle>
+                              </DialogHeader>
+                              <ul className="w-full">
+                                  {["18. HbA1c (гликированный Hb)", "119. Лейкоцитарная формула", "11. Альфа-амилаза", "ОБС73. Биохимия крови, расш."].map(item => (
+                                      <li key={item} className="flex w-full items-center justify-between add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                                      <span id="recommendation-product">
+                                          {item}
+                                      </span>
+                                          <Button data-recommendationid={item.split(".").at(0)} size="icon">
+                                              <Plus/>
+                                          </Button>
+                                      </li>
+                                  ))}
+                              </ul>
+                          </DialogContent>
+                      </Dialog>
                   </article>
                   <article data-productid={56} id="product" className="border flex flex-col gap-2 p-2 rounded-xl">
                       <b>56</b>
                       <p>Тиреотропный гормон (ТТГ, тиротропин) (Thyroid Stimulating Hormone, TSH)</p>
-                      <b className="font-bold">Рекомендации</b>
-                      <ul>
-                          <li>
-                              <button data-recommendationid={"АКЦ818"} id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
-                                  АКЦ818. Акция Обследуй щитовидную железу
-                              </button>
-                          </li>
-                      </ul>
+                      <Dialog>
+                          <DialogTrigger asChild>
+                              <Button>1 рекомендация</Button>
+                          </DialogTrigger>
+                          <DialogContent>
+                              <DialogHeader>
+                                  <DialogTitle>Добавить рекомендации</DialogTitle>
+                              </DialogHeader>
+                              <ul>
+                                  {["АКЦ818. Акция Обследуй щитовидную железу"].map(item => (
+                                      <li key={item} className="flex w-full items-center justify-between add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                                      <span  id="recommendation-product">
+                                          {item}
+                                      </span>
+                                          <Button data-recommendationid={item.split(".").at(0)} size="icon">
+                                              <Plus/>
+                                          </Button>
+                                      </li>
+                                  ))}
+                                  <li>
+                                      <button data-recommendationid={"АКЦ818"} id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+
+                                      </button>
+                                  </li>
+                              </ul>
+                          </DialogContent>
+                      </Dialog>
                   </article>
               </div>
           </div>
