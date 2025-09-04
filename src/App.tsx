55d1f9072ed04b087c277253a8209310bba88c50
alex5 <angrymarshall@gmail.com>
2025-09-04 15:09:05 +0300

    metrika

--- src/App.tsx
+++ src/App.tsx
@@ -9,40 +9,40 @@ function App() {
               </div>
               <hr/>
               <div className="flex flex-col gap-6">
-                  <article data-productid={1} id="product" className="border flex flex-col gap-2 p-2 rounded-xl">
+                  <article data-productid={16} id="product" className="border flex flex-col gap-2 p-2 rounded-xl">
                       <b>16</b>
                       <p>Глюкоза (Glucose) A09.05.023.000.01</p>
                       <b className="font-bold">Рекомендации</b>
                       <ul>
                           <li>
-                              <button id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                              <button data-recommendationid={18} id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
                                   18. HbA1c (гликированный Hb)
                               </button>
                           </li>
                           <li >
-                              <button id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                              <button data-recommendationid={119} id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
                                   119. Лейкоцитарная формула
                               </button>
                           </li>
                           <li>
-                              <button id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                              <button data-recommendationid={11} id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
                                   11. Альфа-амилаза
                               </button>
                           </li>
                           <li>
-                              <button id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                              <button data-recommendationid={"ОБС73"} id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
                                   ОБС73. Биохимия крови, расш.
                               </button>
                           </li>
                       </ul>
                   </article>
-                  <article data-productid={2} id="product" className="border flex flex-col gap-2 p-2 rounded-xl">
+                  <article data-productid={56} id="product" className="border flex flex-col gap-2 p-2 rounded-xl">
                       <b>56</b>
                       <p>Тиреотропный гормон (ТТГ, тиротропин) (Thyroid Stimulating Hormone, TSH)</p>
                       <b className="font-bold">Рекомендации</b>
                       <ul>
                           <li>
-                              <button id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                              <button data-recommendationid={"АКЦ818"} id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
                                   АКЦ818. Акция Обследуй щитовидную железу
                               </button>
                           </li>
