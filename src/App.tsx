2abee170ad7c762d60274a1a86fe8e4c3ba63eab
alex5 <angrymarshall@gmail.com>
2025-09-04 11:10:19 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <section className="mx-auto max-w-3xl py-12">
+          <div className="flex flex-col">
+              <article className="bg-neutral-100 flex flex-col gap-2 p-2 rounded-xl">
+                  <b>16</b>
+                  <p>Глюкоза (Glucose) A09.05.023.000.01</p>
+                  <ul>
+                      <li className="font-bold">Рекомендации</li>
+                      <li>18. HbA1c (гликированный Hb)</li>
+                      <li>119. Лейкоцитарная формула</li>
+                      <li>11. Альфа-амилаза</li>
+                      <li>ОБС73. Биохимия крови, расш.</li>
+                  </ul>
+                  <span>3 аналога</span>
+              </article>
+          </div>
+      </section>
+
   )
 }
 
