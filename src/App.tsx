8af7f16336a079ea17ed0fea87f147dcde43d24c
alex5 <angrymarshall@gmail.com>
2025-09-04 13:39:35 +0300

    metrika

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,52 @@ import './App.css'
 function App() {
   return (
       <section className="mx-auto max-w-3xl py-12">
-          <div className="flex flex-col">
-              <article className="border flex flex-col gap-2 p-2 rounded-xl">
-                  <b>16</b>
-                  <p>Глюкоза (Glucose) A09.05.023.000.01</p>
-                  <b className="font-bold">Рекомендации</b>
-                  <ul>
-                      <li>
-                          <button className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
-                              18. HbA1c (гликированный Hb)
-                          </button>
-                      </li>
-                      <li >
-                          <button className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
-                              119. Лейкоцитарная формула
-                          </button>
-                      </li>
-                      <li>
-                          <button className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
-                              11. Альфа-амилаза
-                          </button>
-                      </li>
-                      <li>
-                          <button className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
-                              ОБС73. Биохимия крови, расш.
-                          </button>
-                      </li>
-                  </ul>
-              </article>
+          <div className="flex flex-col gap-6">
+              <div className="border p-2 rounded-xl">
+                  <b>Офис;</b>
+              </div>
+              <hr/>
+              <div className="flex flex-col gap-6">
+                  <article data-productid={1} id="product" className="border flex flex-col gap-2 p-2 rounded-xl">
+                      <b>16</b>
+                      <p>Глюкоза (Glucose) A09.05.023.000.01</p>
+                      <b className="font-bold">Рекомендации</b>
+                      <ul>
+                          <li>
+                              <button id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                                  18. HbA1c (гликированный Hb)
+                              </button>
+                          </li>
+                          <li >
+                              <button id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                                  119. Лейкоцитарная формула
+                              </button>
+                          </li>
+                          <li>
+                              <button id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                                  11. Альфа-амилаза
+                              </button>
+                          </li>
+                          <li>
+                              <button id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                                  ОБС73. Биохимия крови, расш.
+                              </button>
+                          </li>
+                      </ul>
+                  </article>
+                  <article data-productid={2} id="product" className="border flex flex-col gap-2 p-2 rounded-xl">
+                      <b>56</b>
+                      <p>Тиреотропный гормон (ТТГ, тиротропин) (Thyroid Stimulating Hormone, TSH)</p>
+                      <b className="font-bold">Рекомендации</b>
+                      <ul>
+                          <li>
+                              <button id="recommendation-product" className="add-recommendation p-2 hover:bg-neutral-100 active:bg-neutral-200 rounded-xl">
+                                  АКЦ818. Акция Обследуй щитовидную железу
+                              </button>
+                          </li>
+                      </ul>
+                  </article>
+              </div>
           </div>
       </section>
 
